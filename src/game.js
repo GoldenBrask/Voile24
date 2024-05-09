bddfe1e96160a60bb6655092c4254900471d50d6
GoldenBrask <123570837+GoldenBrask@users.noreply.github.com>
2024-05-09 10:45:54 +0200

    async

--- src/game.js
+++ src/game.js
@@ -34,18 +34,20 @@ class Game {
     this.#engine = engine;
   }
 
-  start() {
-    this.initGame();
+  async start() {
+    await this.initGame();
     this.gameLoop();
     this.endGame();
   }
 
-  initGame() {
+  async initGame() {
+    this.#engine.displayLoadingUI();
     this.#gameScene = this.createScene();
-    Inspector.Show(this.#gameScene,Game)
+    Inspector.Show(this.#gameScene, Game)
+    this.#engine.hideLoadingUI();
   }
 
-  endGame() {}
+  endGame() { }
 
   gameLoop() {
     const divFps = document.getElementById("fps");
@@ -62,83 +64,60 @@ class Game {
     // this.#sphere.position.y = Math.sin(this.#phase); // TODO : SERT POUR LE BOUNCE DU BATEAU
   }
 
-  //   createScene() {
-//     const scene = new Scene(this.#engine);
-//     const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-//     camera.setTarget(Vector3.Zero());
-//     camera.attachControl(this.#canvas, true);
-//     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-//     light.intensity = 0.7;
-//     const sphere = MeshBuilder.CreateSphere(
-//       "sphere",
-//       { diameter: 2, segments: 32 },
-//       scene
-//     );
-//     this.#sphere = sphere;
-//     sphere.position.y = 1;
-//     const ground = MeshBuilder.CreateGround(
-//       "ground",
-//       { width: 6, height: 6 },
-//       scene
-//     );
-//     const matGround = new StandardMaterial("boue", scene);
-//     matGround.diffuseColor = new Color3(1, 0.4, 0);
-//     ground.material = matGround;
-//     return scene;
-//   }
+
 
   createScene = function () {
-	const scene = new Scene(this.#engine);
+    const scene = new Scene(this.#engine);
 
-	let camera = new ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, Vector3.Zero(), scene);
-	camera.attachControl(this.#canvas, true);
+    let camera = new ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, Vector3.Zero(), scene);
+    camera.attachControl(this.#canvas, true);
 
-	let light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
-	
-	// Skybox
-	let skybox = Mesh.CreateBox("skyBox", 1000.0, scene);
+    let light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+
+    // Skybox
+    let skybox = Mesh.CreateBox("skyBox", 1000.0, scene);
     let skyboxMaterial = new StandardMaterial("skyBox", scene);
-	skyboxMaterial.backFaceCulling = false;
-	skyboxMaterial.reflectionTexture = new Texture(skyhUrl, scene);
-	skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
-	skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
-	skyboxMaterial.specularColor = new Color3(0, 0, 0);
-	skyboxMaterial.disableLighting = true;
-	skybox.material = skyboxMaterial;
-		
-	// Ground
-	let groundMaterial = new StandardMaterial("groundMaterial", scene);
-	groundMaterial.diffuseTexture = new Texture(floorUrl, scene);
-	groundMaterial.diffuseTexture.uScale = groundMaterial.diffuseTexture.vScale = 4;
-	
-	let ground = Mesh.CreateGround("ground", this.#mapsize, this.#mapsize, 32, scene, false);
-	ground.position.y = -1;
-	ground.material = groundMaterial;
-		
-	// Water
-	let waterMesh = Mesh.CreateGround("waterMesh", this.#mapsize, this.#mapsize, 32, scene, false);
-	
-	let water = new WaterMaterial("water", scene);
-	water.bumpTexture = new Texture(waterUrl, scene);
-	
-	// Water properties
-	water.windForce = -35;
-	water.waveHeight = 0.7;
-	water.windDirection = new Vector2(1, 1);
-	water.waterColor = new Color3(0.1, 0.1, 0.6);
-	water.colorBlendFactor = 0.3;
-	water.bumpHeight = 0.04;
-	water.waveLength = 0.1;
-	
-	// Add skybox and ground to the reflection and refraction
-	water.addToRenderList(skybox);
-	water.addToRenderList(ground);
-	
-	// Assign the water material
-	waterMesh.material = water;
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new Texture(skyhUrl, scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new Color3(0, 0, 0);
+    skyboxMaterial.disableLighting = true;
+    skybox.material = skyboxMaterial;
 
-	return scene;
-}
+    // Ground
+    let groundMaterial = new StandardMaterial("groundMaterial", scene);
+    groundMaterial.diffuseTexture = new Texture(floorUrl, scene);
+    groundMaterial.diffuseTexture.uScale = groundMaterial.diffuseTexture.vScale = 4;
+
+    let ground = Mesh.CreateGround("ground", this.#mapsize, this.#mapsize, 32, scene, false);
+    ground.position.y = -1;
+    ground.material = groundMaterial;
+
+    // Water
+    let waterMesh = Mesh.CreateGround("waterMesh", this.#mapsize, this.#mapsize, 32, scene, false);
+
+    let water = new WaterMaterial("water", scene);
+    water.bumpTexture = new Texture(waterUrl, scene);
+
+    // Water properties
+    water.windForce = -35;
+    water.waveHeight = 0.7;
+    water.windDirection = new Vector2(1, 1);
+    water.waterColor = new Color3(0.1, 0.1, 0.6);
+    water.colorBlendFactor = 0.3;
+    water.bumpHeight = 0.04;
+    water.waveLength = 0.1;
+
+    // Add skybox and ground to the reflection and refraction
+    water.addToRenderList(skybox);
+    water.addToRenderList(ground);
+
+    // Assign the water material
+    waterMesh.material = water;
+
+    return scene;
+  }
 
 
 }
